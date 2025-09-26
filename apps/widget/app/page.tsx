0ffccba244d425dc9b46590b9a4f917ba5862cdf
Bidyut Bhowmick <barunbhowmick727@gmail.com>
2025-09-26 09:05:27 +0530

    feat:turborepo

--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,14 +1,13 @@
-import { Button } from "@workspace/ui/components/button"
 import { add } from "@workspace/math/add"
-import { Input } from "@workspace/ui/components/input"
+import { Button } from "@workspace/ui/components/button"
+
 export default function Page() {
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello apps weeb</h1>
+        <h1 className="text-2xl font-bold">Hello apps widget</h1>
+        {add(5,4)}
         <Button size="sm">Button</Button>
-        {add(2,2)}
-        <Input/>
       </div>
     </div>
   )
